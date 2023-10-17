573534846e0bc85e4fff737f99ceac835d9859bc
valeriobartoletti97 <valerio.bartoletti97@gmail.com>
2023-10-17 15:19:41 +0200

    palindrome exercise finished

--- js/script.js
+++ js/script.js
@@ -1,22 +1,23 @@
 
-const btn = document.querySelector('button')
+const btn = document.querySelector('button');
 const inputEl = document.getElementById('data');
-const resultEl = document.querySelector('.alert')
+const resultEl = document.querySelector('.alert');
 
 /* PALINDROMA */
-
-btn.addEventListener('click',function(){
-   let wordUser = (inputEl.value).toLowerCase();
-   console.log(wordUser)
-   console.log(wordUser.length)
-   let newString = ''
-   for(let i = wordUser.length - 1; i >= 0; i--){
-       console.log(wordUser[i])
-       newString += wordUser[i]
-   }
-   console.log(newString)
-})
-
-
-let stringa = 'casa'
-
+myPalindrome()
+function myPalindrome(){
+    btn.addEventListener('click',function(){
+        let wordUser = (inputEl.value).toLowerCase();
+        let newString = '';
+        for(let i = wordUser.length - 1; i >= 0; i--){
+            newString += wordUser[i];
+        };
+        if(newString === wordUser){
+         resetForm('bg-danger','bg-success');
+         printResult('bg-success','La parola è palindroma');
+        } else {
+         resetForm('bg-success','bg-danger')
+         printResult('bg-danger','La parola non è palindroma. Riprova')
+        };
+     })
+}
